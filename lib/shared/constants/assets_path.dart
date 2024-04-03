9e30572a54c34ec0f43b0d45d475d7b23851cb5f
MwenyaG <163680035+MwenyaG@users.noreply.github.com>
2024-04-03 14:21:21 +0800

    Upload files

---
+++ lib/shared/constants/assets_path.dart
@@ -0,0 +1,13 @@
+class MyAssets {
+  static const String onboradingone = 'assets/images/onboardingone.png';
+  static const String onboradingtwo = 'assets/images/onboardingtwo.png';
+  static const String onboradingthree = 'assets/images/onboardingthree.png';
+  static const String welcomesketch = 'assets/images/wel.png';
+  static const String googleicon = 'assets/icons/google.png';
+  static const String facebookicon = 'assets/icons/facebook.png';
+  static const String profileicon1 = 'assets/icons/s1.png';
+  static const String profileicon2 = 'assets/icons/s2.png';
+  static const String profileicon3 = 'assets/icons/s3.png';
+  static const String profileicon4 = 'assets/icons/s4.png';
+  static const String clipboard = 'assets/icons/s.png';
+}
